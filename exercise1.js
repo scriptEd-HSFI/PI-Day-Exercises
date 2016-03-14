5aff57d509cca3c7e2f93617b27a68204b258410
ewlin <ericwilliamlin@gmail.com>
2016-03-14 11:46:18 -0400

    exercises 1&2

---
+++ exercise1.js
@@ -0,0 +1,35 @@
+/* EXERCISE 1
+/
+/ Directions: 
+/       Write a function that returns an array of 6 random integers between 0 and 60. It's OK if a number repeats. 
+/       
+/       When I call the function randomNumbers(), something like this should be returned: [2,3,16,25,40,60] or [1,10,10,11,29,30]
+/
+/       Input (arguments) : None
+/       Output (return) : Array of 6 random integers between 0 and 60
+/
+/       Tools and functions you'll probably need to use: 
+/           1) While-Loop or For-Loop
+/           2) Math.random(), Math.ceil(n) and/or Math.floor(n)
+/           3) Array.prototype.push(item(s)). You've seen this before as just arrayName.push():
+/                   var imAnArray = []; 
+/                   imAnArray.push(1,2,3); 
+/                   
+/                   In your console: 
+/                   > imAnArray
+/                   < [1,2,3]
+*/
+
+function randomNumbers() {
+    var arrayOfRandomNumbers = []; 
+    
+    
+    
+    /*
+    / CODE HERE
+    */
+    
+    
+    
+    return arrayOfRandomNumbers; 
+}
---
+++ exercise2.js
@@ -0,0 +1,29 @@
+/* EXERCISE 2
+/
+/ Directions: 
+/       Today is 3/14, also known as PI day. 
+/       Write a function that calculates the area of a circle when given a radius
+/       When I call the function calculateArea(radius), the return value will be the area rounded to the closest integer
+/
+/       Input (arguments) : Number -- i.e. the radius of the circle
+/       Output (return) : Number -- i.e. the area of the circle AS AN INTEGER
+/
+/       Tools and functions you'll probably need to use: 
+/           1) Math.PI (it's a PROPERTY of the Math object...similar to the .length of an array or string!)
+/           2) Pick either Math.ceil(n) or Math.floor(n). For this exericse, it doesn't matter if you round up or down. 
+/           3) What's the formula for the area of a circle? 
+*/
+
+function calculateArea(radius) {
+    var area; 
+    
+    
+    
+    /*
+    / CODE HERE
+    */
+    
+    
+    
+    return area; 
+}
